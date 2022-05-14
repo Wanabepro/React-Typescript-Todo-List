c6f02af6acad203a6fe4383b898c298d88045808
Wanabepro <ilyasov_sk@mail.ru>
2022-05-14 16:37:07 +0300

    State mutation fixed, DnD v0 added

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,29 +1,60 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import { ITodo } from './../Interfaces';
 
 interface todoListProps {
     todos: ITodo[]
     togleCompleted(id: number): void
     deleteTodo(id: number): void
+    changeOrder(id: number, draggingTodo: ITodo): void
 }
 
 interface todoProps {
     todo: ITodo
     togleCompleted(id: number): void
     deleteTodo(id: number): void
+    changeOrder(id: number, draggingTodo: ITodo): void
 }
 
+interface dragHandlerProps {
+    event: React.DragEvent
+    todo?: ITodo
+}
 
-const Todo: React.FC<todoProps> = ({ todo, togleCompleted, deleteTodo }) => {
-    const [completed, localToggleCompleted] = useState(todo.completed)
-    useEffect(() => {
-        if (completed !== todo.completed) togleCompleted(todo.id)
-    }, [completed])
+let draggingTodo: ITodo
+
+
+
+const Todo: React.FC<todoProps> = ({ todo, togleCompleted, deleteTodo, changeOrder }) => {
+    const dragStartHandler = (event: React.MouseEvent, todo: ITodo) => {
+        draggingTodo = todo
+    }
+
+    const dragLeaveHandler = (event: React.DragEvent<HTMLLIElement>) => {
+        (event.target as HTMLElement).style.background = 'white'
+    }
+        
+    const dragOverHandler = (event: React.DragEvent<HTMLLIElement>) => {
+        (event.target as HTMLElement).style.background = 'lightgray'
+        event.preventDefault()
+    }
+
+    const dropHandler = (event: React.DragEvent<HTMLLIElement>, todo: ITodo) => {
+        (event.target as HTMLElement).style.background = 'white'
+        event.preventDefault()
+        changeOrder(todo.id, draggingTodo)
+    }
 
     return (
-        <li className='todo'>
+        <li className='todo'
+            draggable={true}
+            onDragStart={event => dragStartHandler(event, todo)}
+            onDragLeave={event => dragLeaveHandler(event)}
+            onDragOver={event => dragOverHandler(event)}
+            onDrop={event => dropHandler(event, todo)}
+            
+        >
             <label>
-                <input type="checkbox" checked={todo.completed} onChange={() => localToggleCompleted(!completed)} />
+                <input type="checkbox" checked={todo.completed} onChange={() => togleCompleted(todo.id)} />
                 <span className={todo.completed ? 'title completed' : 'title'}>{todo.title}</span>
                 <i className='material-icons bucket' onClick={() => deleteTodo(todo.id)}>delete</i>
             </label>
@@ -31,11 +62,12 @@ const Todo: React.FC<todoProps> = ({ todo, togleCompleted, deleteTodo }) => {
     )
 }
 
-const TodoList: React.FC<todoListProps> = ({ todos, togleCompleted, deleteTodo }) => {
+const TodoList: React.FC<todoListProps> = ({ todos, togleCompleted, deleteTodo, changeOrder }) => {
     if (todos.length === 0) return <p className="center-align">No objectives now</p>
+
     return (
         <ul>
-            {todos.map(todo => <Todo key={todo.id} todo={todo} togleCompleted={togleCompleted} deleteTodo={deleteTodo} />)}
+            {todos.map(todo => <Todo key={todo.id} todo={todo} togleCompleted={togleCompleted} deleteTodo={deleteTodo} changeOrder={changeOrder} />)}
 
         </ul>
     )
--- src/pages/TodoPage.tsx
+++ src/pages/TodoPage.tsx
@@ -5,6 +5,7 @@ import { ITodo } from '../Interfaces';
 
 const TodoPage: React.FC = () => {
     const [todos, setTodos] = useState<ITodo[]>(JSON.parse(localStorage.getItem('todos') || '[]'))
+
     useEffect(() => {
         const stored = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[]
         setTodos(stored)
@@ -18,29 +19,41 @@ const TodoPage: React.FC = () => {
         const newTodo = {
             title,
             id: Date.now(),
-            completed: false
+            completed: false,
         }
         setTodos(prev => [newTodo, ...prev])
     }
 
     const togleCompleted = (id: number) => {
-        setTodos(prev => prev.map(todo => {
-            if (todo.id === id) {
-                todo.completed = !todo.completed
-                return { ...todo }
-            }
-            return todo
-        }))
+        setTodos(prev => {
+            const [completedTodo] = prev.filter(todo => todo.id === id)
+
+            return [
+                ...prev.filter(todo => todo.id !== id),
+                {
+                    ...completedTodo,
+                    completed: !completedTodo.completed
+                }
+            ]
+        })
     }
 
     const deleteTodo = (id: number) => {
         setTodos(prev => prev.filter(todo => todo.id !== id))
     }
 
+    const changeOrder = (id: number, draggingTodo: ITodo) => {
+        setTodos(prev => {
+            const copy = prev.filter(todo => todo.id !== draggingTodo.id)
+            const index = copy.findIndex(todo => todo.id === id)
+            return [...copy.slice(0, index), draggingTodo, ...copy.slice(index)]
+        })
+    }
+
     return (
         <>
             <TodoForm addNewTodo={addNewTodo} />
-            <TodoList todos={todos} togleCompleted={togleCompleted} deleteTodo={deleteTodo} />
+            <TodoList todos={todos} togleCompleted={togleCompleted} deleteTodo={deleteTodo} changeOrder={changeOrder} />
         </>
     )
 }
